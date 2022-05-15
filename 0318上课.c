ffefe3c76229fb82ad4b492bbad4697cfd1b50ce
cccedric1108 <93823429+cccedric1108@users.noreply.github.com>
2022-05-15 10:12:49 +0800

    Create 0318上课.c

---
+++ 0318上课.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+//7-1 计算平均分 (20 分)
+
+/*int main(){
+
+    int math = 87, eng = 72, comp = 93, average;
+    average=(math+eng+comp)/3;
+    printf("math = 87, eng = 72, comp = 93, average = %d",average);
+    return 0;
+}
+*/
+
+//7-2 计算华氏温度 (20 分)
+/*
+
+    int f,c;
+    c=26;
+    f=9*c/5+32;
+    printf("celsius = 26, fahr = %d",f);
+
+    return 0;
+}*/
+
+//7-3 两个实数平方和 (20 分)
+
+/*int main(){
+
+    float a,b,c;
+    scanf("%f %f",&a,&b);
+    c=a*a+b*b;
+    printf("%.6f",c);
+    return 0;
+}*/
+
+//7-4 圆球的体积 (20 分)
+ /*
+int main(){
+
+    double r,v,pi;
+    scanf("%lf",&r);
+    pi=3.1415926535897932384626;
+    v=(4/3.0)*r*r*r*pi;
+    printf("%.2lf",v);
+    
+    return 0;
+}*/
+
+//7-5 计算多项式的值 (10 分)
+/*
+int main(){
+
+    double f,x,a,b,c,d;
+    scanf("x=%lf,a=%lf,b=%lf,c=%lf,d=%lf.",&x,&a,&b,&c,&d);
+    f=a*x*x*x+b*x*x+c*x+d;
+    printf("f(%.7lf)=%.7lf",x,f);
+    return 0;
+}*/
+
+
+//7-6 反向输出一个三位数 (10 分)
+
+/*int main(){
+
+    int a,ge,shi,bai;
+    scanf("%d",&a);
+    bai=(a/100);
+    shi=(a-bai*100)/10;
+    ge=(a-bai*100-shi*10)%10;
+    printf("%d%d%d",ge,shi,bai);
+    return 0;
+}*/
+
